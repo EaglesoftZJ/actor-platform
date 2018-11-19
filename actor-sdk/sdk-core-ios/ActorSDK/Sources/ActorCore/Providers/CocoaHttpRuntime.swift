a15330aa5d11ade21144a1e1fe3eb843567df120
dingjm <363712861@qq.com>
2018-11-19 13:57:14 +0800

    Revert "Httpruntime bugfix"
    This reverts commit 995c63e2cdd98375cca6a0affd571515e8a7d067.

--- actor-sdk/sdk-core-ios/ActorSDK/Sources/ActorCore/Providers/CocoaHttpRuntime.swift
+++ actor-sdk/sdk-core-ios/ActorSDK/Sources/ActorCore/Providers/CocoaHttpRuntime.swift
@@ -20,22 +20,14 @@ class CocoaHttpRuntime: NSObject, ARHttpRuntime {
             request.httpMethod = "GET"
             
             NSURLConnection.sendAsynchronousRequest(request as URLRequest, queue: self.queue, completionHandler:{ (response: URLResponse?, data: Data?, error: Error?) -> Void in
-                if error != nil {
-                    resolver.error(ARHTTPError(int: 0))
-                } else {
-                    if let respHttp = response as? HTTPURLResponse {
-                        if (respHttp.statusCode >= 200 && respHttp.statusCode < 300) {
-                            if data != nil {
-                                resolver.result(ARHTTPResponse(code: jint(respHttp.statusCode), withContent: data!.toJavaBytes()))
-                            } else {
-                                resolver.error(ARHTTPError(int: jint(respHttp.statusCode)))
-                            }
-                        } else {
-                            resolver.error(ARHTTPError(int: jint(respHttp.statusCode)))
-                        }
+                if let respHttp = response as? HTTPURLResponse {
+                    if (respHttp.statusCode >= 200 && respHttp.statusCode < 300) {
+                        resolver.result(ARHTTPResponse(code: jint(respHttp.statusCode), withContent: data!.toJavaBytes()))
                     } else {
-                        resolver.error(ARHTTPError(int: 0))
+                        resolver.error(ARHTTPError(int: jint(respHttp.statusCode)))
                     }
+                } else {
+                    resolver.error(ARHTTPError(int: 0))
                 }
             })
         }
